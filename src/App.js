f15881d7d6dcf9577bfdbc8e6797b3486b067594
Nilu patel <nilusingh945164@gmail.com>
2023-07-24 19:26:36 +0530

    reaact commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+//import About from './Componets/About';
+import Navbar from './Componets/Navbar';
+import TextForm from './Componets/TextForm';
+import Alert from './Componets/Alert';
+
+
 
 function App() {
+  const [mode, setmode] = useState('light');
+  const [alert, setalert] = useState(null);
+  const showalert =(message,type)=>{
+    setalert({
+      msg:message,
+      type:type
+    })
+  }
+    const togglemode=()=>{
+      if(mode==='light'){
+        setmode('dark')
+        document.body.style.backgroundColor='gray';
+        showalert("dark mode is enable","succes");
+      }
+      else{
+        setmode('light')
+        document.body.style.backgroundColor='light';
+        showalert("light mode is enable","succes");
+      }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/*  change navbar using props */}
+      {/* <Navbar title="TextUtil" abouttext=" About Textutil" /> */}
+
+      {/* chane name of title and navbar according to us */}
+      {/* <Navbar /> */}
+
+      <Navbar title="TextUtil"  mode={mode}  togglemode={togglemode}/>
+      <Alert   alert={alert}/>
+      <div className="container  my-3"  >
+        <TextForm heading=" Enter the text of analyze below"   mode={mode}/>
+
+      {/* <About/> */}
+
+      </div>
+
+    </>
+
+
   );
 }
 
