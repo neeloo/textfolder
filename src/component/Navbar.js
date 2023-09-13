e081cbeb1068fe28dababa0d26aa77bff5e4f4d8
Nilu patel <nilusingh945164@gmail.com>
2023-09-13 08:49:26 +0530

    react

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,9 +6,8 @@ export default class Navbar extends Component {
         return (
             <div>
                 {/* <nav className="navbar navbar-expand-lg navbar-light bg-light"> */}
-                
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/">NewMonky</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
